b86776ef82965336c0660be4a48a68a9d83e162b
akshayah3 <akshaynukala95@gmail.com>
2016-06-28 16:56:04 -0400

    First commit

---
+++ star1.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 15:47:31 2016
+
+"""
+import math
+
+sigma = 5.6704e-5
+
+class Star1:
+    """
+    Star 1 is a small sphere if it represents neutron star, or an invisible point mass if it
+    represents a black hole.  In either case, it is much smaller than all other dimensions in the
+    system and is unresolved by any of the grids used in calculating the light curves.
+    
+    Parameters
+    ==========
+    
+    L: Luminosity of the compact Star.
+    
+    T: Temperature of the compact Star.
+    
+    
+    """
+
+    def __init__(self, L, T):
+        self.L = L
+        self.T = T
+
+    @property
+    def luminosity(self):
+        """
+        Returns the luminosity of the compact Star.
+        """
+        return self.L
+
+    @property
+    def temperature(self):
+        """
+        Returns the temperature of the compact Star.
+        """
+        return self.T
+
+    @property
+    def radius(self):
+        """
+        Returns the radius of the compact star.
+        """
+        radius = self.L/((((self.T)**4) * sigma)* math.pi)
+        return radius
+
+    def Star1TotFlux(self, distance):
+        """
+        This function returns the integrated flux from star 1.
+        The flux has been integrated over wavelength and over 
+        the surface of the star.
+        """
+        totalflux = (self.L/ 4*math.pi*distance*distance)
+        return totalflux
+        
+    def MeanRocheRadius(self, q):
+        """
+        This function returns the mean radius of the Roche lobe
+        in units of the separation of the two stars, e.g., <R_lobe>/a.
+        using Eggleton's (1983, ApJ, 268, 368) formula.
+        q = (Mass of star inside the Roche lobe) / (Mass of the other star)
+        """
+        x = math.pow(q, 0.33)
+        radius = 0.49 * x * x / ( 0.6 * x * x + math.log(1 + x) )
+        return radius
+
+    def Star1Flambda(self, filter = "Square", minlambda, maxlambda):
+        """
+        This function returns the contribution of star 1 to the observed 
+        spectrum at wavelength lambda.  Note that the wavelengths must be in 
+        centimeters but the returned flux is in ergs/sec/cm^2/Angstrom.
+        """        
+        
+        if filter == "Square":
+            flux = ( self.L / (4.0 * (self.T)**4) ) * BBSquareIntensity( self.T, minlambda, maxlambda )
+        else:
+            flux = ( self.L / (4.0 * (self.T)**4) ) * BBFilterIntensity( self.T, filter )
+        return flux
---
+++ utility.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 16:53:23 2016
+
+"""
+
